a5bad792f3ec07d89187a77c7a807d1389ac9acb
dykyiivan <dykyiivan@gmail.com>
2020-03-18 18:38:15 +0200

    Fix Bugs

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-$(function(){
+$(function () {
+ // Scroll
+  var margin = 0;
+  $("a").click(function () {
+    $("html, body").animate({
+      scrollTop: $($(this).attr("href")).offset().top + margin + "px"
+    }, {
+      duration: 1600,
+      easing: "swing"
+    });
+    return false;
+  });
+
+  //Slider
   $('.reviews-slider').slick({
-     slidesToShow: 3,
-     prevArrow: '<button type="button" class="slick-prev"><img src="img/prev-arrow.png"></button>',
-     nextArrow: '<button type="button" class="slick-next"><img src="img/next-arrow.png"></button>',
-     responsive: [
+    slidesToShow: 3,
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/prev-arrow.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/next-arrow.png"></button>',
+    responsive: [
       {
         breakpoint: 1024,
         settings: {
@@ -14,33 +27,59 @@ $(function(){
           arrows: false
         }
       },
-      
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: false,
-        slidesToShow: 1,
-        dots: true
+
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          slidesToShow: 1,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          slidesToShow: 1,
+          dots: true
+
+        }
       }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: false,
-        slidesToShow: 1,
-        dots: true
-  
-      }
-    }
-  ]
+    ]
   });
 
-  $('.header__burger').click(function() {
+  //Burger Menu
+  $('.header__burger').click(function () {
     $('.header__burger,.menu').toggleClass('active');
     $('body').toggleClass('lock');
   });
+  $(".menu__close").on("click", function () {
+    $('.header__burger,.menu').toggleClass('active');
+  });
 
-  new WOW().init();
 });
+
+//Wow Js
+new WOW().init();
+
+//Modal
+$("[data-modal=call]").on("click", function () {
+  $(".overlay, #call").fadeIn();
+});
+$(".modal__close").on("click", function () {
+  $(".overlay, #call").fadeOut("slow");
+});
+
+// To Top Button
+let btn = $('#to-top__link');
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+
